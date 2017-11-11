5d3f3948b0cbd983d5c0051f67131b38d1631a61
patricklindenberg <patrick@heapdump.com>
2017-11-11 10:10:33 +0100

    add cast to angle definition add blue version of Davies1900h

--- src/LindenbergResearch.hpp
+++ src/LindenbergResearch.hpp
@@ -6,18 +6,24 @@ using namespace rack;
 extern Plugin *plugin;
 
 struct SimpleFilterWidget : ModuleWidget {
-	SimpleFilterWidget();
+    SimpleFilterWidget();
 };
 
 struct LRBasicKnob : SVGKnob {
-	LRBasicKnob() {
-		minAngle = -0.83f*M_PI;
-		maxAngle = 0.83f*M_PI;
-	}
+    LRBasicKnob() {
+        minAngle = -0.83f * (float) M_PI;
+        maxAngle = 0.83f * (float) M_PI;
+    }
 };
 
 struct LRBasicKnobWhite : LRBasicKnob {
-	LRBasicKnobWhite() {
-		setSVG(SVG::load(assetPlugin(plugin, "res/LRBasicKnobWhite.svg")));
-	}
+    LRBasicKnobWhite() {
+        setSVG(SVG::load(assetPlugin(plugin, "res/LRBasicKnobWhite.svg")));
+    }
+};
+
+struct Davies1900hBlue : LRBasicKnob {
+    Davies1900hBlue() {
+        setSVG(SVG::load(assetPlugin(plugin, "res/Davies1900hBlue.svg")));
+    }
 };
