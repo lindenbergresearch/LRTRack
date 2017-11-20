ac1ffe70ff05bbe4c88f51af5075fb290c64f643
patricklindenberg <patrick@heapdump.com>
2017-11-20 16:10:03 +0100

    change note

--- src/LindenbergResearch.cpp
+++ src/LindenbergResearch.cpp
@@ -9,11 +9,8 @@ void init(rack::Plugin *p) {
     //plugin->name = "Lindenberg Research";
     p->website = "https://github.com/lindenbergresearch/LRTRack";
 
-    
     p->addModel(createModel<SimpleFilterWidget>("LindenbergResearch", "LPFilter24dB", "24dB Lowpass Filter", FILTER_TAG));
-    p->addModel(createModel<BlankPanelWidget>("LindenbergResearch", "BlankPanel", "Blank Panel 26TE", UTILITY_TAG));
+    p->addModel(createModel<BlankPanelWidget>("LindenbergResearch", "BlankPanel", "Blank Panel 20TE", UTILITY_TAG));
     p->addModel(createModel<ReShaperWidget>("LindenbergResearch", "ReShaper", "ReShaper Wavefolder", FILTER_TAG));
-    
-
 }
 
