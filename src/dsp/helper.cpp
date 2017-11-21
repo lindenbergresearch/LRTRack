fda438ed41035c81fea93252fb860b039753a065
patricklindenberg <patrick@heapdump.com>
2017-11-21 16:19:36 +0100

    add fast sin approximation helper

--- src/dsp/helper.cpp
+++ src/dsp/helper.cpp
@@ -2,6 +2,28 @@
 #include "engine.hpp"
 
 /**
+ * Fast sin approximation
+ * @param angle
+ * @return
+ */
+float fastSin(float angle) {
+    float sqr = angle * angle;
+    float result = -2.39e-08f;
+    result *= sqr;
+    result += 2.7526e-06f;
+    result *= sqr;
+    result -= 1.98409e-04f;
+    result *= sqr;
+    result += 8.3333315e-03f;
+    result *= sqr;
+    result -= 1.666666664e-01f;
+    result *= sqr;
+    result += 1.0f;
+    result *= angle;
+    return result;
+}
+
+/**
  * Clip signal at bottom by value
  * @param in Sample input
  * @param clip Clipping value
@@ -44,5 +66,7 @@ float getPhaseIncrement(float frq) {
 
 
 float BLIT(float N, float phase, float inc) {
+    float tmp = sin((clipl(N - 1, 0.f) + 0.5f) * phase) * 1.f / sin(0.5f * phase) - 1.f;
 
+    return tmp * 0.5f;
 }
