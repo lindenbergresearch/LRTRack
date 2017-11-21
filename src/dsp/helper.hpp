9dfb1a7944d37ecb7b4b4da64aff0175b6017618
patricklindenberg <patrick@heapdump.com>
2017-11-21 11:34:17 +0100

    add function stub

--- src/dsp/helper.hpp
+++ src/dsp/helper.hpp
@@ -1,10 +1,12 @@
 #pragma once
 
 #include <cmath>
+#include "rack.hpp"
 
 namespace Rack {
     #define PI 3.14159265359f
     #define TWOPI 6.28318530718f
 
     float wrapTWOPI(float n);
+    float getPhaseIncrement(float frq);
 }
