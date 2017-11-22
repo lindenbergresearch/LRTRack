c548fe8d6d704da124e383a673b1772c4da6d99d
patricklindenberg <patrick@heapdump.com>
2017-11-22 11:20:53 +0100

    initial commit

---
+++ create_release.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+hdiutil create -volname WhatYouWantTheDiskToBeNamed -srcfolder /Users/patricklindenberg/Documents/Rack/plugins/LindenbergResearch/ -ov -format UDZO foobar.dmg
---
+++ src/Release.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define MAJOR 0
+#define MINOR 0
+#define PATCH 3
+
