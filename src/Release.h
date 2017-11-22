55f955518602061aa0eff053bc304f33a83ee427
patricklindenberg <patrick@heapdump.com>
2017-11-22 12:05:18 +0100

    add version and release information

--- src/Release.h
+++ src/Release.h
@@ -1,6 +1,21 @@
 #pragma once
 
-#define MAJOR 0
-#define MINOR 0
-#define PATCH 3
+#include <string>
+
+#define VERSION_MAJOR 0
+#define VERSION_MINOR 0
+#define VERSION_PATCH 3
+
+/**
+ * @brief This structure holds some basic release and version information
+ */
+struct Release {
+    std::string version =
+            "v" + std::to_string(VERSION_MAJOR) +
+            "." + std::to_string(VERSION_MINOR) +
+            "." + std::to_string(VERSION_PATCH);
+
+    std::string vendor = "Lindenberg Research Tec.";
+    std::string title = "LRT Rack modules";
+};
 
