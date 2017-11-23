bce69e3319cb30cbbcb46a3aeaf4f6d64f476309
patricklindenberg <patrick@heapdump.com>
2017-11-23 15:17:39 +0100

    fix OO design in files

--- src/dsp/Oscillator.cpp
+++ src/dsp/Oscillator.cpp
@@ -1,108 +1,20 @@
 #include "dsp/helper.hpp"
+#include "dsp/Oscillator.hpp"
 
 using namespace rack;
 
 
-struct Oscillator {
-    float freq = 440.f; // oscillator frequency
-    float pw = 0.5f;    // pulse-width value
-    float phase = 0.f;  // current phase
-    float incr = 0.f;   // current phase increment for PLL
+void Oscillator::reset() {
+    freq = 440.f;
+    pw = 0.5f;
+    phase = 0.f;
+    incr = 0.f;
 
-    /* currents of waveforms */
-    float ramp = 0.f;
-    float saw = 0.f;
-    float pulse = 0.f;
-    float sawtri = 0.f;
-    float tri = 0.f;
-
-    /**
-     * @brief Proccess next sample for output
-     */
-    virtual void proccess();
+    invalidate();
+}
 
 
-    /**
-     * @brief ReCompute states on change
-     */
-    virtual void invalidate();
+Oscillator::Oscillator(float freq, float pw) : freq(freq), pw(pw) {}
 
 
-    /**
-     * @brief Reset oscillator
-     */
-    void reset() {
-        freq = 440.f;
-        pw = 0.5f;
-        phase = 0.f;
-        incr = 0.f;
-
-        invalidate();
-    }
-
-
-    /**
-     * @brief Change PulseWidth of oscillator
-     * @param pw
-     */
-    void setPW(float pw) {
-        this->pw = pw;
-        invalidate();
-    }
-
-
-    /**
-     * @brief Change frequency of oscillator
-     * @param frq
-     */
-    void setFrequency(float freq) {
-        this->freq = freq;
-        invalidate();
-    }
-
-
-    /**
-     * @brief Get current value of ramp waveform
-     * @return
-     */
-    float getRamp() {
-        return ramp;
-    }
-
-
-    /**
-     * @brief Get current value of saw waveform
-     * @return
-     */
-    float getSaw() {
-        return saw;
-    }
-
-
-    /**
-     * @brief Get current value of pulse waveform
-     * @return
-     */
-    float getPulse() {
-        return pulse;
-    }
-
-
-    /**
-     * @brief Get current value of triangle-saw waveform
-     * @return
-     */
-    float getSawTri() {
-        return sawtri;
-    }
-
-
-    /**
-     * @brief Get current value of triangle waveform
-     * @return
-     */
-    float getTri() {
-        return tri;
-    }
-
-};
+Oscillator::~Oscillator() {}
--- src/dsp/Oscillator.hpp
+++ src/dsp/Oscillator.hpp
@@ -1,2 +1,98 @@
 #pragma once
 
+struct Oscillator {
+    float freq = 440.f; // oscillator frequency
+    float pw = 0.5f;    // pulse-width value
+    float phase = 0.f;  // current phase
+    float incr = 0.f;   // current phase increment for PLL
+
+    /* currents of waveforms */
+    float ramp = 0.f;
+    float saw = 0.f;
+    float pulse = 0.f;
+    float sawtri = 0.f;
+    float tri = 0.f;
+
+    Oscillator(float freq, float pw);
+    virtual ~Oscillator();
+
+    /**
+     * @brief Proccess next sample for output
+     */
+    virtual void proccess();
+
+
+    /**
+     * @brief ReCompute states on change
+     */
+    virtual void invalidate();
+
+
+    void reset();
+
+
+    /**
+     * @brief Change PulseWidth of oscillator
+     * @param pw
+     */
+    void setPW(float pw) {
+        this->pw = pw;
+        invalidate();
+    }
+
+
+    /**
+     * @brief Change frequency of oscillator
+     * @param frq
+     */
+    void setFrequency(float freq) {
+        this->freq = freq;
+        invalidate();
+    }
+
+
+    /**
+     * @brief Get current value of ramp waveform
+     * @return
+     */
+    float getRamp() {
+        return ramp;
+    }
+
+
+    /**
+     * @brief Get current value of saw waveform
+     * @return
+     */
+    float getSaw() {
+        return saw;
+    }
+
+
+    /**
+     * @brief Get current value of pulse waveform
+     * @return
+     */
+    float getPulse() {
+        return pulse;
+    }
+
+
+    /**
+     * @brief Get current value of triangle-saw waveform
+     * @return
+     */
+    float getSawTri() {
+        return sawtri;
+    }
+
+
+    /**
+     * @brief Get current value of triangle waveform
+     * @return
+     */
+    float getTri() {
+        return tri;
+    }
+
+};
