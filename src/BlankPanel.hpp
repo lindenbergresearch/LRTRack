9764a52fbad28c578ef7e3c57615981de6501827
patricklindenberg <patrick@heapdump.com>
2017-11-26 11:02:04 +0100

    reformat and add lcd fonts

--- src/BlankPanel.hpp
+++ src/BlankPanel.hpp
@@ -1,7 +1,8 @@
+#pragma once
+
 #include "rack.hpp"
 
-using namespace rack;
-
-#define MODULE_WIDTH 20.f
-#define MODULE_HEIGHT RACK_GRID_HEIGHT
-
+namespace rack {
+    #define MODULE_WIDTH 20.f
+    #define MODULE_HEIGHT RACK_GRID_HEIGHT
+}
--- src/LindenbergResearch.hpp
+++ src/LindenbergResearch.hpp
@@ -4,120 +4,126 @@
 #include "asset.hpp"
 #include "widgets.hpp"
 
-/* true type font for LCD display emulation */
-#define LCD_SEGMENT_FONT "res/digital7.ttf"
-#define LCD_COLOR_FG nvgRGBA(0x00, 0x2F, 0xAF, 0xFF)
+namespace rack {
+
+    /* true type fonts for LCD display emulation */
+    #define LCD_FONT_DIG7 "res/digital7.ttf"
+    #define LCD_FONT_MAX "res/Maxter Board St.ttf"
+    #define LCD_FONT_MAX "res/pocket_calcuatlor_tt.ttf"
+
+    /* LCD default color */
+    #define LCD_COLOR_FG nvgRGBA(0xFA, 0x10, 0x04, 0xFF)
+
+    extern Plugin *plugin;
 
 
-using namespace rack;
-
-extern Plugin *plugin;
-
-
-struct SimpleFilterWidget : ModuleWidget {
-    SimpleFilterWidget();
-};
+    struct SimpleFilterWidget : ModuleWidget {
+        SimpleFilterWidget();
+    };
 
 
-struct BlankPanelWidget : ModuleWidget {
-    BlankPanelWidget();
-};
+    struct BlankPanelWidget : ModuleWidget {
+        BlankPanelWidget();
+    };
 
 
-struct ReShaperWidget : ModuleWidget {
-    ReShaperWidget();
-};
+    struct ReShaperWidget : ModuleWidget {
+        ReShaperWidget();
+    };
 
 
-struct VCOWidget : ModuleWidget {
-    VCOWidget();
-};
+    struct VCOWidget : ModuleWidget {
+        VCOWidget();
+    };
 
 
 /**
  * @brief Emulation of an LCD monochrome display
  */
-struct LCDWidget : Label {
-    std::shared_ptr<Font> gLCDFont;
-    NVGcolor fg;
-    NVGcolor bg;
+    struct LCDWidget : Label {
+        std::shared_ptr<Font> gLCDFont;
+        NVGcolor fg;
+        NVGcolor bg;
+        unsigned char length = 0;
 
 
-    /**
-     * @brief Constructor
-     */
-    LCDWidget(NVGcolor fg) {
-        /** load LCD ttf font */
-        gLCDFont = Font::load(assetPlugin(plugin, LCD_SEGMENT_FONT));
+        /**
+         * @brief Constructor
+         */
+        LCDWidget(NVGcolor fg, unsigned char length) {
+            /** load LCD ttf font */
+            gLCDFont = Font::load(assetPlugin(plugin, LCD_FONT_DIG7));
 
-        unsigned char r = (unsigned char) (fg.r * 255);
-        unsigned char g = (unsigned char) (fg.g * 255);
-        unsigned char b = (unsigned char) (fg.b * 255);
+            unsigned char r = (unsigned char) (fg.r * 255);
+            unsigned char g = (unsigned char) (fg.g * 255);
+            unsigned char b = (unsigned char) (fg.b * 255);
+
+            LCDWidget::length = length;
+
+            LCDWidget::fg = fg;
+            LCDWidget::bg = nvgRGBA(r - 0x20, g - 0x20, b - 0x20, 0x20);
+        }
 
 
-        LCDWidget::fg = fg;
-        LCDWidget::bg = nvgRGBA(r - 0x20, g - 0x20, b - 0x20, 0x20);
-    }
-
-
-    /**
-     * @brief Draw LCD display
-     * @param vg
-     */
-    void draw(NVGcontext *vg) override;
-};
+        /**
+         * @brief Draw LCD display
+         * @param vg
+         */
+        void draw(NVGcontext *vg) override;
+    };
 
 
 /**
  * @brief Basic knob definition
  */
-struct LRBasicKnob : SVGKnob {
-    LRBasicKnob() {
-        minAngle = -0.83f * (float) M_PI;
-        maxAngle = 0.83f * (float) M_PI;
-    }
-};
+    struct LRBasicKnob : SVGKnob {
+        LRBasicKnob() {
+            minAngle = -0.83f * (float) M_PI;
+            maxAngle = 0.83f * (float) M_PI;
+        }
+    };
 
 
 /**
  * @brief Basic middle-sized knob
  */
-struct LRBigKnobWhite : LRBasicKnob {
-    LRBigKnobWhite() {
-        setSVG(SVG::load(assetPlugin(plugin, "res/BigKnob.svg")));
-    }
-};
+    struct LRBigKnobWhite : LRBasicKnob {
+        LRBigKnobWhite() {
+            setSVG(SVG::load(assetPlugin(plugin, "res/BigKnob.svg")));
+        }
+    };
 
 
 /**
  * @brief Blue version of the Davies1900h
  */
-struct LRBasicKnobWhite : LRBasicKnob {
-    LRBasicKnobWhite() {
-        setSVG(SVG::load(assetPlugin(plugin, "res/LRBasicKnobWhite.svg")));
-    }
-};
+    struct LRBasicKnobWhite : LRBasicKnob {
+        LRBasicKnobWhite() {
+            setSVG(SVG::load(assetPlugin(plugin, "res/LRBasicKnobWhite.svg")));
+        }
+    };
 
 
 /**
  * @brief Alternative IO Port
  */
-struct IOPort : SVGPort {
-    IOPort() {
-        background->svg = SVG::load(assetPlugin(plugin, "res/IOPortB.svg"));
-        background->wrap();
-        box.size = background->box.size;
-    }
-};
+    struct IOPort : SVGPort {
+        IOPort() {
+            background->svg = SVG::load(assetPlugin(plugin, "res/IOPortB.svg"));
+            background->wrap();
+            box.size = background->box.size;
+        }
+    };
 
 
 /**
  * @brief Alternative screw head A
  */
-struct ScrewDarkA : SVGScrew {
-    ScrewDarkA() {
-        sw->svg = SVG::load(assetPlugin(plugin, "res/ScrewDarkA.svg"));
-        sw->wrap();
-        box.size = sw->box.size;
-    }
-};
+    struct ScrewDarkA : SVGScrew {
+        ScrewDarkA() {
+            sw->svg = SVG::load(assetPlugin(plugin, "res/ScrewDarkA.svg"));
+            sw->wrap();
+            box.size = sw->box.size;
+        }
+    };
+}
