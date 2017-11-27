38d4c7498f7cf5d7f3408d5381facf674b1908cb
patricklindenberg <patrick@heapdump.com>
2017-11-27 09:07:43 +0100

    reformat

--- src/dsp/Oscillator.cpp
+++ src/dsp/Oscillator.cpp
@@ -1,196 +1,204 @@
 #include "helper.hpp"
 #include "Oscillator.hpp"
 
-namespace rack {
-    /**
-     * @brief Set oscillator state back
-     */
-    void BLITOscillator::reset() {
-        freq = 440.f;
-        pw = 1.f;
-        phase = 0.f;
-        incr = 0.f;
+using namespace rack;
 
-        /* force recalculation of variables */
-        invalidate();
+
+/**
+ * @brief Set oscillator state back
+ */
+void BLITOscillator::reset() {
+    freq = 440.f;
+    pw = 1.f;
+    phase = 0.f;
+    incr = 0.f;
+    harmonics = BLIT_HARMONICS;
+
+    /* force recalculation of variables */
+    invalidate();
+}
+
+
+/**
+ * @brief Default constructor
+ */
+BLITOscillator::BLITOscillator() {
+    reset();
+}
+
+
+/**
+ * @brief Default destructor
+ */
+BLITOscillator::~BLITOscillator() {}
+
+
+/**
+ * @brief Get current frequency
+ * @return
+ */
+float BLITOscillator::getFrequency() const {
+    return freq;
+}
+
+
+/**
+ * @brief Set frequency
+ * @param freq
+ */
+void BLITOscillator::setFrequency(float freq) {
+    BLITOscillator::freq = freq;
+
+    /* force recalculation of variables */
+    invalidate();
+}
+
+
+/**
+ * @brief Get current pulse-width
+ * @return
+ */
+float BLITOscillator::getPulseWidth() const {
+    return pw;
+}
+
+
+/**
+ * @brief Set current pulse-width
+ * @param pw
+ */
+void BLITOscillator::setPulseWidth(float pw) {
+    if (pw < 0.1f) {
+        BLITOscillator::pw = 0.1f;
+        return;
     }
 
-
-    /**
-     * @brief Default constructor
-     */
-    BLITOscillator::BLITOscillator() {
-        reset();
+    if (pw > 1.f) {
+        BLITOscillator::pw = 1.f;
+        return;
     }
 
+    BLITOscillator::pw = pw;
 
-    /**
-     * @brief Default destructor
-     */
-    BLITOscillator::~BLITOscillator() {}
-
-
-    /**
-     * @brief Get current frequency
-     * @return
-     */
-    float BLITOscillator::getFrequency() const {
-        return freq;
-    }
+    /* force recalculation of variables */
+    invalidate();
+}
 
 
-    /**
-     * @brief Set frequency
-     * @param freq
-     */
-    void BLITOscillator::setFrequency(float freq) {
-        BLITOscillator::freq = freq;
-
-        /* force recalculation of variables */
-        invalidate();
-    }
+/**
+ * @brief Ramp waveform current
+ * @return
+ */
+float BLITOscillator::getRampWave() const {
+    return ramp;
+}
 
 
-    /**
-     * @brief Get current pulse-width
-     * @return
-     */
-    float BLITOscillator::getPulseWidth() const {
-        return pw;
-    }
+/**
+ * @brief Saw waveform current
+ * @return
+ */
+float BLITOscillator::getSawWave() const {
+    return saw;
+}
 
 
-    /**
-     * @brief Set current pulse-width
-     * @param pw
-     */
-    void BLITOscillator::setPulseWidth(float pw) {
-        if (pw < 0.f) {
-            BLITOscillator::pw = 0.f;
-            return;
-        }
-
-        if (pw > 1.f) {
-            BLITOscillator::pw = 1.f;
-            return;
-        }
-
-        BLITOscillator::pw = pw;
-
-        /* force recalculation of variables */
-        invalidate();
-    }
+/**
+ * @brief Pulse waveform current
+ * @return
+ */
+float BLITOscillator::getPulseWave() const {
+    return pulse;
+}
 
 
-    /**
-     * @brief Ramp waveform current
-     * @return
-     */
-    float BLITOscillator::getRampWave() const {
-        return ramp;
-    }
+/**
+ * @brief SawTri waveform current
+ * @return
+ */
+float BLITOscillator::getSawTriWave() const {
+    return sawtri;
+}
 
 
-    /**
-     * @brief Saw waveform current
-     * @return
-     */
-    float BLITOscillator::getSawWave() const {
-        return saw;
-    }
+/**
+ * @brief Triangle waveform current
+ * @return
+ */
+float BLITOscillator::getTriangleWave() const {
+    return tri;
+}
 
 
-    /**
-     * @brief Pulse waveform current
-     * @return
-     */
-    float BLITOscillator::getPulseWave() const {
-        return pulse;
-    }
+/**
+ * @brief Process band-limited oscillator
+ */
+void BLITOscillator::proccess() {
+    /* phase locked loop */
+    phase = wrapTWOPI(incr + phase);
+
+    /* pulse width */
+    float w = pw * (float) M_PI;
+
+    /* get impulse train */
+    float blit1 = BLIT(N, phase);
+    float blit2 = BLIT(N, wrapTWOPI(w + phase));
+
+    /* feed integrator */
+    int1.add(blit1, incr);
+    int2.add(blit2, incr);
+
+    /* integrator delta */
+    float delta = int1.value - int2.value;
+
+    /* 3rd integrator */
+    float beta = int3.add(delta, incr) * 6.f;
+
+    /* compute RAMP waveform */
+    ramp = int1.value - 0.1f;
+    /* compute pulse waveform */
+    pulse = delta * 1.6f;
+    /* compute SAW waveform */
+    saw = ramp * -1.f;
+    /* compute triangle */
+    tri = (float) M_PI / w * beta;
+    /* compute sawtri */
+    sawtri = saw + beta;
+
+    /* adjust output levels */
+    ramp *= 10;
+    saw *= 10;
+    pulse *= 5;
+    sawtri = dcb.filter(sawtri) * 5;
+    tri *= 6;
+}
 
 
-    /**
-     * @brief SawTri waveform current
-     * @return
-     */
-    float BLITOscillator::getSawTriWave() const {
-        return sawtri;
-    }
+/**
+ * @brief ReCompute basic parameter
+ */
+void BLITOscillator::invalidate() {
+    incr = getPhaseIncrement(freq);
+    N = (int) floorf(harmonics / freq);
+}
 
 
-    /**
-     * @brief Triangle waveform current
-     * @return
-     */
-    float BLITOscillator::getTriangleWave() const {
-        return tri;
-    }
+/**
+ * @brief Get current BLIT harmonics
+ * @return
+ */
+float BLITOscillator::getHarmonics() const {
+    return harmonics;
+}
 
 
-    /**
-     * @brief Process band-limited oscillator
-     */
-    void BLITOscillator::proccess() {
-        /* phase locked loop */
-        phase = wrapTWOPI(incr + phase);
+/**
+ * @brief Set BLIT harmonics
+ * @param harmonics
+ */
+void BLITOscillator::setHarmonics(float harmonics) {
+    BLITOscillator::harmonics = harmonics;
 
-        /* pulse width */
-        float w = pw * (float)M_PI;
-
-        /* get impulse train */
-        float blit1 = BLIT(N, phase);
-        float blit2 = BLIT(N, wrapTWOPI(w + phase));
-
-        /* feed integrator */
-        int1.add(blit1, incr);
-        int2.add(blit2, incr);
-
-        /* integrator delta */
-        float delta = int1.value - int2.value;
-
-        /* 3rd integrator */
-        float beta = int3.add(delta, incr) * 6.f;
-
-        /* compute RAMP waveform */
-        ramp = int1.value - 0.1f;
-        /* compute pulse waveform */
-        pulse = delta * 1.6f;
-        /* compute SAW waveform */
-        saw = ramp * -1.f;
-        /* compute triangle */
-        tri = (float)M_PI / w * beta;
-        /* compute sawtri */
-        sawtri = saw + beta;
-    }
-
-
-    /**
-     * @brief ReCompute basic parameter
-     */
-    void BLITOscillator::invalidate() {
-        incr = getPhaseIncrement(freq);
-        N = (int) floorf(harmonics / freq);
-    }
-
-
-    /**
-     * @brief Get current BLIT harmonics
-     * @return
-     */
-    float BLITOscillator::getHarmonics() const {
-        return harmonics;
-    }
-
-
-    /**
-     * @brief Set BLIT harmonics
-     * @param harmonics
-     */
-    void BLITOscillator::setHarmonics(float harmonics) {
-        BLITOscillator::harmonics = harmonics;
-
-        /* force recalculation of variables */
-        invalidate();
-    }
-
+    /* force recalculation of variables */
+    invalidate();
 }
