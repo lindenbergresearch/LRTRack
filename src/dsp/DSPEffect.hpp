5db02ebc0f5488aad0369c5c0fd138abf335517f
patricklindenberg <patrick@heapdump.com>
2017-12-21 11:19:25 +0100

    add virtual functions

--- src/dsp/DSPEffect.hpp
+++ src/dsp/DSPEffect.hpp
@@ -1,14 +1,23 @@
-#ifndef LRT_DSPEFFECT_HPP
-#define LRT_DSPEFFECT_HPP
+#pragma once
 
-/**
- * @brief
- */
-struct DSPEffect {
+namespace rack {
 
-    virtual void invalidate();
+    /**
+     * @brief Base class for all signal processors
+     */
+    struct DSPEffect {
+
+        /**
+         * @brief Method for mark parameters as invalidate to trigger recalculation
+         */
+        virtual void invalidate() {};
 
 
-};
+        /**
+         * @brief Process one step and return the computed sample
+         * @return
+         */
+        virtual void process() {};
+    };
 
-#endif //LRT_DSPEFFECT_HPP
+}
