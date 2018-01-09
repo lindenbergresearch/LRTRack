34bb677bfb16aed74ebce5db8fe1a2ec66e7c1be
patricklindenberg <patrick@heapdump.com>
2018-01-09 16:54:57 +0100

    cleanup

--- src/dsp/LadderFilter.cpp
+++ src/dsp/LadderFilter.cpp
@@ -32,6 +32,7 @@ void LadderFilter::process() {
     for (int i = 0; i < os.factor; i++) {
         float x = os.up[i];
 
+        // non linear feedback with nice saturation
         x -= fastatan(b5 * q);
 
         t1 = b1;
@@ -48,21 +49,14 @@ void LadderFilter::process() {
 
         b5 = ((b4 + t2) * p - b5 * f);
 
-
-        // b4 = clampf(b4 - b4 * b4 * b4 * 0.16666667f, -0.9f, 0.9f);// + rnd.nextFloat(-10e-8f, +10e-8f);
-        //  b4 = lpf.filter(tanh(b4)) + rnd.nextFloat(-10e-8f, +10e-8f);
-        //  b4 = (b4 - quadraticBipolar(b4)*0.1) + rnd.nextFloat(-10e-8f, +10e-8f);
-
-        // b4 = clampf(b4 - (b4 * b4 * b4 / 6.f), -0.4f, 0.4f);// + rnd.nextFloat(-10e-8f, +10e-8f);
-
-
+        // saturate and add very low noise to have self oscillation with no input and high res
         b0 = fastatan(x + noise.nextFloat(NOISE_GAIN));
 
+        // overdrive with fastatan, which folds back the waves at high input and creates a noisy bright sound
         os.data[LOWPASS][i] = fastatan(b5 * (1 + drive * 50));
     }
 
     lpOut = os.getDownsampled(LOWPASS);
-
 }
 
 
@@ -93,13 +87,10 @@ void LadderFilter::setFrequency(float frequency) {
 
 
 /**
- * @brief Update non-linear resonance factor
+ * @brief Update resonance factor
  */
 void LadderFilter::updateResExp() {
     resExp = clampf(resonance, 0, 1.5f);
-    // resExp = resExp * (1 - (drive / 3));
-    // add some curve to resonance to avoid aliasing at high frequency
-    // resExp *= (-0.50 * frequency + 1.1);
 }
 
 
