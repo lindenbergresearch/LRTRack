e77a6f183444288936f076e60d9bb3f24630fc59
patricklindenberg <patrick@heapdump.com>
2018-01-12 18:19:06 +0100

    change version

--- src/Release.h
+++ src/Release.h
@@ -5,7 +5,7 @@
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 0
 #define VERSION_PATCH 5
-#define VERSION_BUILD 1213
+#define VERSION_BUILD 177
 
 /**
  * @brief This structure holds some basic release and version information
