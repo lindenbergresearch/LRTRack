d1f463baaebafd8e54fd6f2d461f4094b6c06656
patricklindenberg <patrick@heapdump.com>
2018-01-15 08:14:20 +0100

    cleanup

--- src/LindenbergResearch.cpp
+++ src/LindenbergResearch.cpp
@@ -22,7 +22,7 @@ void init(rack::Plugin *p) {
     p->addModel(createModel<BlankPanelWidgetM1>("Lindenberg Research", "BlankPanel 02", "Blank Panel 12TE", UTILITY_TAG));
     p->addModel(createModel<ReShaperWidget>("Lindenberg Research", "ReShaper", "ReShaper Wavefolder", FILTER_TAG));
     // not ready jet
-    //p->addModel(createModel<VCOWidget>("Lindenberg Research", "VCO", "Voltage Controlled Oscillator", OSCILLATOR_TAG));
+    p->addModel(createModel<VCOWidget>("Lindenberg Research", "VCO", "Voltage Controlled Oscillator", OSCILLATOR_TAG));
 }
 
 
@@ -51,51 +51,6 @@ void LCDWidget::draw(NVGcontext *vg) {
     nvgFillColor(vg, fg);
     nvgTextBox(vg, 0, 0, 220, text.c_str(), nullptr);
 }
-
-
-/**
- * @brief    --- TO BE REMOVED IN FUTURE ---
- * @param vg
-
-void LRLightWidget::draw(NVGcontext *vg) {
-    float radius = box.size.x / 2.0f;
-    float oradius = radius + 15.0f;
-
-    color.r = clampf(color.r, 0.0f, 1.0f);
-    color.g = clampf(color.g, 0.0f, 1.0f);
-    color.b = clampf(color.b, 0.0f, 1.0f);
-    color.a = clampf(color.a, 0.0f, 1.0f);
-
-    // Solid
-    nvgBeginPath(vg);
-    nvgCircle(vg, radius, radius, radius);
-    nvgFillColor(vg, bgColor);
-    nvgFill(vg);
-
-    // Border
-    nvgStrokeWidth(vg, 1.0f);
-    NVGcolor borderColor = bgColor;
-    borderColor.a *= 0.5f;
-    nvgStrokeColor(vg, borderColor);
-    nvgStroke(vg);
-
-    // Inner glow
-    nvgGlobalCompositeOperation(vg, NVG_LIGHTER);
-    nvgFillColor(vg, color);
-    nvgFill(vg);
-
-    // Outer glow
-    nvgBeginPath(vg);
-    nvgRect(vg, radius - oradius, radius - oradius, 2 * oradius, 2 * oradius);
-    NVGpaint paint;
-    NVGcolor icol = color;
-    icol.a *= 0.10f;
-    NVGcolor ocol = color;
-    ocol.a = 0.05f;
-    paint = nvgRadialGradient(vg, radius, radius, radius, oradius, icol, ocol);
-    nvgFillPaint(vg, paint);
-    nvgFill(vg);
-}*/
 
 
 void LRRedLight::draw(NVGcontext *vg) {
