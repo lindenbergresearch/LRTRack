35e8fa7e6e314b23626002031a9d9278560ee468
patricklindenberg <patrick@heapdump.com>
2018-01-23 10:11:26 +0100

    fix tags

--- src/LindenbergResearch.cpp
+++ src/LindenbergResearch.cpp
@@ -18,9 +18,9 @@ void init(rack::Plugin *p) {
     p->website = "https://github.com/lindenbergresearch/LRTRack";
 
     p->addModel(createModel<AlmaFilterWidget>("Lindenberg Research", "VCF", "ALMA Lowpass Filter", FILTER_TAG));
-    p->addModel(createModel<BlankPanelWidget>("Lindenberg Research", "BlankPanel 01", "Blank Panel 20TE", UTILITY_TAG));
-    p->addModel(createModel<BlankPanelWidgetM1>("Lindenberg Research", "BlankPanel 02", "Blank Panel 12TE", UTILITY_TAG));
-    p->addModel(createModel<ReShaperWidget>("Lindenberg Research", "ReShaper", "ReShaper Wavefolder", FILTER_TAG));
+    p->addModel(createModel<BlankPanelWidget>("Lindenberg Research", "BlankPanel 01", "Blank Panel 20TE", BLANK_TAG));
+    p->addModel(createModel<BlankPanelWidgetM1>("Lindenberg Research", "BlankPanel 02", "Blank Panel 12TE", BLANK_TAG));
+    p->addModel(createModel<ReShaperWidget>("Lindenberg Research", "ReShaper", "ReShaper Wavefolder", WAVESHAPER_TAG));
     p->addModel(createModel<ReShaperWidget>("Lindenberg Research", "LPFilter24dB", "24dB Lowpass Filter", FILTER_TAG));
 
     // createModel<SimpleFilterWidget>(plugin, "LPFilter24dB", "24dB Lowpass Filter");
