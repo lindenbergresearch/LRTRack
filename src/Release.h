071123b8e614dfaf6a7d2ceda4b5aac771d418bf
patricklindenberg <patrick@heapdump.com>
2018-01-24 08:25:06 +0100

    turn vendor and version information to prep. definition

--- src/Release.h
+++ src/Release.h
@@ -1,22 +1,8 @@
 #pragma once
 
-#include <string>
-
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 5
-#define VERSION_PATCH 185
+#define VERSION_PATCH 204
 
-/**
- * @brief This structure holds some basic release and version information
- */
-struct Release {
-    std::string version =
-            "v" + std::to_string(VERSION_MAJOR) +
-            "." + std::to_string(VERSION_MINOR) +
-            "." + std::to_string(VERSION_PATCH);
-
-
-    std::string vendor = "Lindenberg Research Tec.";
-    std::string title = "LRT Rack modules";
-};
-
+#define VENDOR "Lindenberg Research Tec."
+#define TITLE "LRT Rack modules"
