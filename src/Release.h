e42ca9e4cf5219a1c7c99661ad5593d0391ce1f7
patricklindenberg <patrick@heapdump.com>
2018-01-24 08:40:03 +0100

    change version for release

--- src/Release.h
+++ src/Release.h
@@ -2,7 +2,7 @@
 
 #define VERSION_MAJOR "0"
 #define VERSION_MINOR "5"
-#define VERSION_PATCH "204"
+#define VERSION_PATCH "210"
 #define VERSION_STR "Version " VERSION_MAJOR "." VERSION_MINOR "." VERSION_PATCH
 
 #define VENDOR "Lindenberg Research Tec."
