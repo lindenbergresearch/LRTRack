adce3e00effd81b80fd97bc4add97ee2f8c8b370
patricklindenberg <patrick@heapdump.com>
2018-01-25 18:04:54 +0100

    add TPT and ZDF structures

--- src/dsp/MS20zdf.hpp
+++ src/dsp/MS20zdf.hpp
@@ -7,4 +7,45 @@
 
 namespace rack {
 
+    struct TPT {
+        float y0, y;
+
+
+        float getY(float x, float g) {
+            float gx = g * x;
+            y0 = y;
+
+            y = gx + y0 + gx;
+
+            return y0;
+        }
+    };
+
+
+    struct ZDF {
+        float y0, y;
+        float s;
+        TPT tpt;
+
+
+        void compute(float x, float g) {
+            y0 = y;
+            s = tpt.getY(x - y0, g);
+            y = g * x + s;
+        }
+
+    };
+
+
+    struct MS20zdf : DSPEffect {
+    private:
+        // cutoff frequency and peak
+        float frq, peak;
+
+
+    public:
+
+    };
+
+
 }
