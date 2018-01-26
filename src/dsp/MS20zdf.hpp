f28e33785bd782d14c1101333a67fee8868d1a04
patricklindenberg <patrick@heapdump.com>
2018-01-26 10:11:04 +0100

    add doc's

--- src/dsp/MS20zdf.hpp
+++ src/dsp/MS20zdf.hpp
@@ -7,10 +7,19 @@
 
 namespace rack {
 
+    /**
+     * @brief MS20 Topology Preserving Transform
+     */
     struct TPT {
         float y0, y;
 
 
+        /**
+         * @brief Get current Y value
+         * @param x
+         * @param g
+         * @return
+         */
         float getY(float x, float g) {
             float gx = g * x;
             y0 = y;
@@ -22,12 +31,20 @@ namespace rack {
     };
 
 
+    /**
+     * @brief Zero Delay Feedback
+     */
     struct ZDF {
         float y0, y;
         float s;
         TPT tpt;
 
 
+        /**
+         * @brief Compute ZDF
+         * @param x
+         * @param g
+         */
         void compute(float x, float g) {
             y0 = y;
             s = tpt.getY(x - y0, g);
