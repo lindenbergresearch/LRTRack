90185b0fa16581bf73676a25f8d310a4e091b28b
patricklindenberg <patrick@heapdump.com>
2018-03-04 02:35:32 +0100

    drop manual panel widths (now set by svg)

--- src/LindenbergResearch.hpp
+++ src/LindenbergResearch.hpp
@@ -6,15 +6,6 @@
 #include "widgets.hpp"
 #include "LRComponents.hpp"
 
-/* panel dimensions */
-#define BLANKPANEL_WIDTH 18.f
-#define BLANKPANEL_MARK_I_WIDTH 12.f
-#define FILTER_WIDTH 12.f
-#define MS20_WIDTH 12.f
-#define OSCILLATOR_WIDTH 12.f
-#define RESHAPER_WIDTH 8.f
-#define SIMPLEFILTER_WIDTH 10.f
-
 using namespace rack;
 
 extern Model *modelSimpleFilter;
