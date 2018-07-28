521de6804b469b38bf513c06c4fac7d52115634c
patricklindenberg <patrick@heapdump.com>
2018-07-28 12:05:26 +0200

    add chebyshev polynom generator

--- src/dsp/DSPMath.hpp
+++ src/dsp/DSPMath.hpp
@@ -365,3 +365,30 @@ inline float cubicShape(float x) {
 inline float atanShaper(float x) {
     return x / (1.f + (0.28f * x * x));
 }
+
+
+/** Generate chebyshev polynoms
+ * @brief
+ * @param x Input sample
+ * @param A ?
+ * @param order Polynom order
+ * @return
+ */
+inline float chebyshev(float x, float A[], int order) {
+    // To = 1
+    // T1 = x
+    // Tn = 2.x.Tn-1 - Tn-2
+    // out = sum(Ai*Ti(x)) , i C {1,..,order}
+    float Tn_2 = 1.0f;
+    float Tn_1 = x;
+    float Tn;
+    float out = A[0] * Tn_1;
+
+    for (int n = 2; n <= order; n++) {
+        Tn = 2.0f * x * Tn_1 - Tn_2;
+        out += A[n - 1] * Tn;
+        Tn_2 = Tn_1;
+        Tn_1 = Tn;
+    }
+    return out;
+}
