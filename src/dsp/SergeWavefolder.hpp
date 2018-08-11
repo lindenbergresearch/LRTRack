c0d2e20030dadd62c5e020ee5ebb067427df90a8
patricklindenberg <patrick@heapdump.com>
2018-08-11 16:37:52 +0200

    add computation for the serge wavefolder

--- src/dsp/SergeWavefolder.hpp
+++ src/dsp/SergeWavefolder.hpp
@@ -1,7 +1,31 @@
 #pragma once
 
 
+#include "WaveShaper.hpp"
+#include "HQTrig.hpp"
+
+#define SERGE_R1 33e3
+#define SERGE_IS 10e-9
+#define SERGE_VT 25.864
+
+#define SERGE_THRESHOLD 10e-10
+
 namespace dsp {
+
+    struct SergeWFStage {
+    private:
+
+        double fn1, xn1;
+        double a, b, d;
+
+    public:
+
+        SergeWFStage();
+
+        double compute(double x);
+
+    };
+
 
     class SergeWavefolder {
 
