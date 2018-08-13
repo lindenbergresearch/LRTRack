794ac88fb00daea7f1eaa616b2bf30bc4b12f156
patricklindenberg <patrick@heapdump.com>
2018-08-13 17:23:21 +0200

    rename constant

--- src/dsp/SergeWavefolder.cpp
+++ src/dsp/SergeWavefolder.cpp
@@ -8,17 +8,17 @@ double SergeWFStage::compute(double x) {
     double l, u, ln, fn, xn;
 
     l = sign(x);
-    u = (SERGE_R1 * SERGE_IS) / (SERGE_N * SERGE_VT) * pow(M_E, (l * x) / (SERGE_N * SERGE_VT));
+    u = (SERGE_R1 * SERGE_IS) / (SERGE_ETA * SERGE_VT) * pow(M_E, (l * x) / (SERGE_ETA * SERGE_VT));
     ln = lambert_W_Fritsch(u);
-    fn = x * x / 2 - SERGE_VT * SERGE_N * SERGE_N * SERGE_VT * (ln * (ln + 2));
+    fn = SERGE_VT * SERGE_ETA * SERGE_ETA * SERGE_VT * (ln * (ln + 2)) - x * x / 2;
 
     // Check for ill-conditioning
-    if (abs(x - xn1) < SERGE_THRESHOLD) {
+    if (abs(x - xn1) < 10e-12) {
         // Compute Averaged Wavefolder Output
         xn = 0.5 * (x + xn1);
-        u = (SERGE_R1 * SERGE_IS) / (SERGE_N * SERGE_VT) * pow(M_E, (l * xn) / (SERGE_VT * SERGE_N));
+        u = (SERGE_R1 * SERGE_IS) / (SERGE_ETA * SERGE_VT) * pow(M_E, (l * xn) / (SERGE_VT * SERGE_ETA));
         ln = lambert_W_Fritsch(u);
-        out = xn - 2 * l * SERGE_N * SERGE_VT * ln;
+        out = 2 * l * SERGE_ETA * SERGE_VT * ln - xn;
     } else {
         // Apply AA Form
         out = (fn - fn1) / (x - xn1);
