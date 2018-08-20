7b691c85c27e952bb65bcb380d9aca59aa88e416
patricklindenberg <patrick@heapdump.com>
2018-08-20 22:07:59 +0200

    add saturator math

--- src/dsp/Saturator.cpp
+++ src/dsp/Saturator.cpp
@@ -1,5 +1,43 @@
-//
-// Created by Patrick Lindenberg on 20.08.18.
-//
-
 #include "Saturator.hpp"
+
+using namespace dsp;
+
+
+Saturator::Saturator(float sr) : WaveShaper(sr) {
+    init();
+    noise = new Noise;
+    tanh1 = new HQTanh(sr, 4);
+}
+
+
+void Saturator::init() {
+    WaveShaper::rs = new Resampler<1>(4);
+}
+
+
+void Saturator::process() {
+    WaveShaper::process();
+}
+
+
+void Saturator::invalidate() {}
+
+
+double Saturator::compute(double x) {
+    double out;
+    double in = clampd(x, -10, 10);
+
+    in *= clampd(gain, 0., 20.); // add gain
+    in += clampd(bias * 2, -6., 6.); // add bias
+
+    in *= SATURATOR_GAIN;
+
+    in = tanh1->next(in);
+
+    in *= 1 / SATURATOR_GAIN;
+    if (blockDC) in = dc->filter(in);
+
+    out = in + noise->nextFloat(SATURATOR_NOISE);
+
+    return out * 10.;
+}
--- src/dsp/Saturator.hpp
+++ src/dsp/Saturator.hpp
@@ -2,10 +2,24 @@
 
 #include "WaveShaper.hpp"
 
+#define SATURATOR_NOISE 0.0017234327
+#define SATURATOR_GAIN 0.2
 
 namespace dsp {
 
-    class Saturator : WaveShaper {
+    struct Saturator : WaveShaper {
+
+        Noise *noise;
+
+
+    public:
+
+        explicit Saturator(float sr);
+
+        void init() override;
+        void invalidate() override;
+        void process() override;
+        double compute(double x) override;
 
     };
 
