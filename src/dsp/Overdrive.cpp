fce0e840b069f80ce165d9dffa98b00c31f5c010
patricklindenberg <patrick@heapdump.com>
2018-08-24 20:43:20 +0200

    initial commit

---
+++ src/dsp/Overdrive.cpp
@@ -0,0 +1,43 @@
+#include "Overdrive.hpp"
+
+using namespace dsp;
+
+
+Overdrive::Overdrive(float sr) : WaveShaper(sr) {
+    init();
+    noise = new Noise;
+    tanh1 = new HQTanh(sr, 4);
+}
+
+
+void Overdrive::init() {
+    WaveShaper::rs = new Resampler<1>(1);
+}
+
+
+void Overdrive::process() {
+    WaveShaper::process();
+}
+
+
+void Overdrive::invalidate() {}
+
+
+double Overdrive::compute(double x) {
+    double out;
+    double in = clampd(x, -SHAPER_MAX_VOLTS, SHAPER_MAX_VOLTS);
+
+    in *= clampd(gain, 0., 20.); // add gain
+    in += clampd(bias * 2, -SHAPER_MAX_BIAS, SHAPER_MAX_BIAS); // add bias
+
+    in *= OVERDRIVE_GAIN;
+
+    in = tanh1->next(in);
+
+   // in *= 1 / OVERDRIVE_GAIN * 0.3;
+   // if (blockDC) in = dc->filter(in);
+
+    out = in + noise->nextFloat(OVERDRIVE_NOISE);
+
+    return out;
+}
---
+++ src/dsp/Overdrive.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "WaveShaper.hpp"
+
+#define OVERDRIVE_NOISE 0.0017234327
+#define OVERDRIVE_GAIN 0.05
+
+namespace dsp {
+
+    struct Overdrive : WaveShaper {
+
+        Noise *noise;
+
+
+    public:
+
+        explicit Overdrive(float sr);
+
+        void init() override;
+        void invalidate() override;
+        void process() override;
+        double compute(double x) override;
+
+    };
+
+}
