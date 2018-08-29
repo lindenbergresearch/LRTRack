8589d9940b0dd9df8886c36c9bb2bdd3ceb7cbd0
patricklindenberg <patrick@heapdump.com>
2018-08-29 07:45:17 +0200

    decrease drift amount

--- src/dsp/Oscillator.hpp
+++ src/dsp/Oscillator.hpp
@@ -8,8 +8,8 @@
 #define LFO_MODE -4
 #define CV_BOUNDS 10.f
 #define DETUNE_AMOUNT  2.0f
-#define DRIFT_AMOUNT 0.8f
-#define DRIFT_FREQ 0.0005f
+#define DRIFT_AMOUNT 0.5f
+#define DRIFT_FREQ 0.00005f
 #define DRIFT_VARIANZ 0.004
 
 namespace dsp {
