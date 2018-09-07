2937c451b7cbc565b2a16be41c68f39be52d9933
patricklindenberg <patrick@heapdump.com>
2018-09-07 18:16:16 +0200

    add basic elements for testing

--- src/DiodeVCF.cpp
+++ src/DiodeVCF.cpp
@@ -7,12 +7,17 @@ using namespace lrt;
 
 struct DiodeVCF : Module {
     enum ParamIds {
+        FREQUENCY_PARAM,
+        RES_PARAM,
+        SATURATE_PARAM,
         NUM_PARAMS
     };
     enum InputIds {
+        FILTER_INPUT,
         NUM_INPUTS
     };
     enum OutputIds {
+        FILTER_OUTPUT,
         NUM_OUTPUTS
     };
     enum LightIds {
@@ -22,6 +27,10 @@ struct DiodeVCF : Module {
 
     DiodeVCF() : Module(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS) {}
 
+
+    LRBigKnob *frqKnob = NULL;
+    LRMiddleKnob *resKnob = NULL;
+    LRMiddleKnob *saturateKnob = NULL;
 
     void step() override;
 };
@@ -42,7 +51,7 @@ struct DiodeVCFWidget : LRModuleWidget {
 
 DiodeVCFWidget::DiodeVCFWidget(DiodeVCF *module) : LRModuleWidget(module) {
     panel = new LRPanel();
-    panel->setBackground(SVG::load(assetPlugin(plugin, "res/panels/DiodeVCF.svg")));
+    panel->setBackground(SVG::load(assetPlugin(plugin, "res/panels/DiodeLadderVCF.svg")));
     addChild(panel);
 
     box.size = panel->box.size;
@@ -54,8 +63,26 @@ DiodeVCFWidget::DiodeVCFWidget(DiodeVCF *module) : LRModuleWidget(module) {
     addChild(Widget::create<ScrewDarkA>(Vec(box.size.x - 30, 366)));
     // ***** SCREWS **********
 
+    // ***** MAIN KNOBS ******
+    module->frqKnob = LRKnob::create<LRBigKnob>(Vec(102, 64.9), module, DiodeVCF::FREQUENCY_PARAM, 0.f, 1.f, 1.f);
+    module->resKnob = LRKnob::create<LRMiddleKnob>(Vec(110, 160.8), module, DiodeVCF::RES_PARAM, 0.0f, 1.0, 0.0f);
+    module->saturateKnob = LRKnob::create<LRMiddleKnob>(Vec(110, 230.6), module, DiodeVCF::SATURATE_PARAM, 0.f, 1.0, 0.0f);
+
+    addParam(module->frqKnob);
+    addParam(module->resKnob);
+    addParam(module->saturateKnob);
+    // ***** MAIN KNOBS ******
+
+
+    // ***** INPUTS **********
+    addInput(Port::create<LRIOPort>(Vec(17.999, 326.05), Port::INPUT, module, DiodeVCF::FILTER_INPUT));
+    // ***** INPUTS **********
+
+    // ***** OUTPUTS *********
+    addOutput(Port::create<LRIOPort>(Vec(58.544, 326.05), Port::OUTPUT, module, DiodeVCF::FILTER_OUTPUT));
+    // ***** OUTPUTS *********
 
 }
 
 
-Model *modelDiodeVCF = Model::create<DiodeVCF, DiodeVCFWidget>("Lindenberg Research", "DIODE VCF", "Laika DiodeLadder ", BLANK_TAG);
+Model *modelDiodeVCF = Model::create<DiodeVCF, DiodeVCFWidget>("Lindenberg Research", "DIODE VCF", "Laika Diode-Ladder Filter", FILTER_TAG);
