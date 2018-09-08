9e2ed7a5cffc2366ca690388917da1a827acdd36
patricklindenberg <patrick@heapdump.com>
2018-09-08 16:18:44 +0200

    add debug lcd and pass parameters

--- src/DiodeVCF.cpp
+++ src/DiodeVCF.cpp
@@ -28,6 +28,7 @@ struct DiodeVCF : Module {
     DiodeVCF() : Module(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS) {}
 
 
+    LRLCDWidget *lcd = new LRLCDWidget(nvgRGBAf(0.2, 0.99, 0.33, 1.0), 12, "%00004.3f Hz", LRLCDWidget::NUMERIC);
     dsp::DiodeLadderFilter *lpf = new dsp::DiodeLadderFilter(engineGetSampleRate());
 
     LRBigKnob *frqKnob = NULL;
@@ -40,14 +41,18 @@ struct DiodeVCF : Module {
 
 
 void DiodeVCF::step() {
-    lpf->setFc(params[FREQUENCY_PARAM].value);
-    lpf->setK(params[RES_PARAM].value);
+    lpf->setFrequency(params[FREQUENCY_PARAM].value);
+    lpf->setResonance(params[RES_PARAM].value * 16);
+    lpf->setSaturation(params[SATURATE_PARAM].value * 5 + 1);
 
-    lpf->setIn(inputs[FILTER_INPUT].value);
+    lcd->value = 20 * powf(1000.f, params[FREQUENCY_PARAM].value);
 
+    lpf->setIn(inputs[FILTER_INPUT].value / 10.f);
+
+    lpf->invalidate();
     lpf->process();
 
-    outputs[FILTER_OUTPUT].value = lpf->getOut();
+    outputs[FILTER_OUTPUT].value = lpf->getOut() * 10.f;
 }
 
 
@@ -81,8 +86,8 @@ DiodeVCFWidget::DiodeVCFWidget(DiodeVCF *module) : LRModuleWidget(module) {
 
     // ***** MAIN KNOBS ******
     module->frqKnob = LRKnob::create<LRBigKnob>(Vec(102, 64.9), module, DiodeVCF::FREQUENCY_PARAM, 0.f, 1.f, 1.f);
-    module->resKnob = LRKnob::create<LRMiddleKnob>(Vec(110, 160.8), module, DiodeVCF::RES_PARAM, 0.0f, 1.0, 0.0f);
-    module->saturateKnob = LRKnob::create<LRMiddleKnob>(Vec(110, 230.6), module, DiodeVCF::SATURATE_PARAM, 0.f, 1.0, 0.0f);
+    module->resKnob = LRKnob::create<LRMiddleKnob>(Vec(100, 160.8), module, DiodeVCF::RES_PARAM, 0.0f, 1.0, 0.0f);
+    module->saturateKnob = LRKnob::create<LRMiddleKnob>(Vec(115, 230.6), module, DiodeVCF::SATURATE_PARAM, 0.f, 1.0, 0.0f);
 
     addParam(module->frqKnob);
     addParam(module->resKnob);
@@ -97,6 +102,12 @@ DiodeVCFWidget::DiodeVCFWidget(DiodeVCF *module) : LRModuleWidget(module) {
     // ***** OUTPUTS *********
     addOutput(Port::create<LRIOPort>(Vec(58.544, 326.05), Port::OUTPUT, module, DiodeVCF::FILTER_OUTPUT));
     // ***** OUTPUTS *********
+
+    // **** SETUP LCD ********
+    module->lcd->box.pos = Vec(14, 155);
+    module->lcd->format = "%4.3f Hz";
+    addChild(module->lcd);
+    // **** SETUP LCD ********
 
 }
 
