6b288b2da82becc9469f995bb2c6bd9a1baef773
patricklindenberg <patrick@heapdump.com>
2018-09-12 16:10:44 +0200

    add resizable ability

--- src/BlankPanelEmpty.cpp
+++ src/BlankPanelEmpty.cpp
@@ -21,7 +21,6 @@ struct BlankPanelEmpty : Module {
 
 
     BlankPanelEmpty() : Module(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS) {
-        icons = new FontIconWidget();
     }
 
 
@@ -39,25 +38,56 @@ void BlankPanelEmpty::step() {
  * @brief Blank Panel Mark I
  */
 struct BlankPanelEmptyWidget : LRModuleWidget {
+    ModuleResizeWidget *resizeWidget, *resizeWidgetRight;
+    ScrewDarkA *screw1, *screw2;
+
     BlankPanelEmptyWidget(BlankPanelEmpty *module);
+
+    void step() override;
 };
 
 
 BlankPanelEmptyWidget::BlankPanelEmptyWidget(BlankPanelEmpty *module) : LRModuleWidget(module) {
-    panel = new LRPanel();
-    panel->setBackground(SVG::load(assetPlugin(plugin, "res/panels/BlankPanelM1.svg")));
+    panel = new LRPanel(0, -200);
+    // panel->setBackground(SVG::load(assetPlugin(plugin, "res/panels/BlankPanelM1.svg")));
+    panel->setColorOnly();
     addChild(panel);
 
+    module->icons = new FontIconWidget(60.f);
     addChild(module->icons);
 
     box.size = panel->box.size;
 
+    resizeWidget = new ModuleResizeWidget(box.size.x);
+    resizeWidgetRight = new ModuleResizeWidget(box.size.x);
+    resizeWidgetRight->right = true;
+    addChild(resizeWidget);
+    addChild(resizeWidgetRight);
+
+
     // ***** SCREWS **********
     addChild(Widget::create<ScrewDarkA>(Vec(15, 1)));
-    addChild(Widget::create<ScrewDarkA>(Vec(box.size.x - 30, 1)));
     addChild(Widget::create<ScrewDarkA>(Vec(15, 366)));
-    addChild(Widget::create<ScrewDarkA>(Vec(box.size.x - 30, 366)));
+
+    screw1 = Widget::create<ScrewDarkA>(Vec(box.size.x - 30, 1));
+    screw2 = Widget::create<ScrewDarkA>(Vec(box.size.x - 30, 366));
+
+    addChild(screw1);
+    addChild(screw2);
     // ***** SCREWS **********
+}
+
+
+void BlankPanelEmptyWidget::step() {
+    panel->box.size = box.size;
+    resizeWidgetRight->box.pos.x = box.size.x - resizeWidgetRight->box.size.x;
+    resizeWidgetRight->box.pos.y = box.size.y - resizeWidgetRight->box.size.y;
+    resizeWidget->box.pos.y = box.size.y - resizeWidget->box.size.y;
+
+    screw1->box.pos.x = box.size.x - 30;
+    screw2->box.pos.x = box.size.x - 30;
+
+    ModuleWidget::step();
 }
 
 
