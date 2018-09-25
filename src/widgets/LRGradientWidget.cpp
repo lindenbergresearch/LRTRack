96d0d3b0e8296cbba984b9d11609d55d2b8b0ce0
patricklindenberg <patrick@heapdump.com>
2018-09-25 17:09:24 +0200

    reformat

--- src/widgets/LRGradientWidget.cpp
+++ src/widgets/LRGradientWidget.cpp
@@ -2,9 +2,11 @@
 
 namespace lrt {
 
+/**
+ * @brief Draw routines for gradient effect
+ * @param vg
+ */
 void LRGradientWidget::draw(NVGcontext *vg) {
-    FramebufferWidget::draw(vg);
-
     nvgBeginPath(vg);
     nvgRect(vg, -margin, -margin, box.size.x + margin * 2, box.size.y + margin * 2);
     NVGpaint paint = nvgLinearGradient(vg, v1.x, v1.y, v2.x, v2.y, innerColor, outerColor);
