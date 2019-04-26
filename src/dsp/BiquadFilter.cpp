39402dad761a328b3bb2c0f94f1e516d1f7d4965
patricklindenberg <patrick@heapdump.com>
2019-04-26 11:58:11 +0200

    add multi biquad filter

---
+++ src/dsp/BiquadFilter.cpp
@@ -0,0 +1,173 @@
+/*                                                                     *\
+**       __   ___  ______                                              **
+**      / /  / _ \/_  __/                                              **
+**     / /__/ , _/ / /    Lindenberg                                   **
+**    /____/_/|_| /_/  Research Tec.                                   **
+**                                                                     **
+**                                                                     **
+**	  https://github.com/lindenbergresearch/LRTRack	                   **
+**    heapdump@icloud.com                                              **
+**		                                                               **
+**    Sound Modules for VCV Rack                                       **
+**    Copyright 2017/2018 by Patrick Lindenberg / LRT                  **
+**                                                                     **
+**    For Redistribution and use in source and binary forms,           **
+**    with or without modification please see LICENSE.                 **
+**                                                                     **
+\*                                                                     */
+
+#include <math.h>
+#include "BiquadFilter.hpp"
+
+
+void dsp::Biquad::init() {}
+
+
+dsp::Biquad::Biquad(float sr) : dsp::DSPEffect(sr) {
+    type = LOWPASS;
+    a0 = 1.0;
+    a1 = a2 = b1 = b2 = 0.0;
+    Fc = 0.50;
+    Q = 0.707;
+    peakGain = 0.0;
+    z1 = z2 = 0.0;
+    init();
+}
+
+
+dsp::Biquad::Biquad(BiquadType type, double Fc, double Q, double peakGainDB, float sr) : dsp::DSPEffect(sr) {
+    setBiquad(type, Fc, Q, peakGainDB);
+    z1 = z2 = 0.0;
+}
+
+
+dsp::Biquad::~Biquad() {
+}
+
+
+void dsp::Biquad::setType(BiquadType type) {
+    this->type = type;
+    invalidate();
+}
+
+
+void dsp::Biquad::setQ(double Q) {
+    this->Q = Q;
+    invalidate();
+}
+
+
+void dsp::Biquad::setFc(double Fc) {
+    this->Fc = Fc;
+    invalidate();
+}
+
+
+void dsp::Biquad::setPeakGain(double peakGainDB) {
+    this->peakGain = peakGainDB;
+    invalidate();
+}
+
+
+void dsp::Biquad::setBiquad(BiquadType type, double Fc, double Q, double peakGainDB) {
+    this->type = type;
+    this->Q = Q;
+    this->Fc = Fc;
+    setPeakGain(peakGainDB);
+}
+
+
+void dsp::Biquad::invalidate() {
+    double norm;
+    double V = pow(10, fabs(peakGain) / 20.0);
+    double K = tan(M_PI * Fc / sr);
+    switch (this->type) {
+        case LOWPASS:
+            norm = 1 / (1 + K / Q + K * K);
+            a0 = K * K * norm;
+            a1 = 2 * a0;
+            a2 = a0;
+            b1 = 2 * (K * K - 1) * norm;
+            b2 = (1 - K / Q + K * K) * norm;
+            break;
+
+        case HIGHPASS:
+            norm = 1 / (1 + K / Q + K * K);
+            a0 = 1 * norm;
+            a1 = -2 * a0;
+            a2 = a0;
+            b1 = 2 * (K * K - 1) * norm;
+            b2 = (1 - K / Q + K * K) * norm;
+            break;
+
+        case BANDPASS:
+            norm = 1 / (1 + K / Q + K * K);
+            a0 = K / Q * norm;
+            a1 = 0;
+            a2 = -a0;
+            b1 = 2 * (K * K - 1) * norm;
+            b2 = (1 - K / Q + K * K) * norm;
+            break;
+
+        case NOTCH:
+            norm = 1 / (1 + K / Q + K * K);
+            a0 = (1 + K * K) * norm;
+            a1 = 2 * (K * K - 1) * norm;
+            a2 = a0;
+            b1 = a1;
+            b2 = (1 - K / Q + K * K) * norm;
+            break;
+
+        case PEAK:
+            if (peakGain >= 0) {    // boost
+                norm = 1 / (1 + 1 / Q * K + K * K);
+                a0 = (1 + V / Q * K + K * K) * norm;
+                a1 = 2 * (K * K - 1) * norm;
+                a2 = (1 - V / Q * K + K * K) * norm;
+                b1 = a1;
+                b2 = (1 - 1 / Q * K + K * K) * norm;
+            } else {    // cut
+                norm = 1 / (1 + V / Q * K + K * K);
+                a0 = (1 + 1 / Q * K + K * K) * norm;
+                a1 = 2 * (K * K - 1) * norm;
+                a2 = (1 - 1 / Q * K + K * K) * norm;
+                b1 = a1;
+                b2 = (1 - V / Q * K + K * K) * norm;
+            }
+            break;
+        case LOWSHELF:
+            if (peakGain >= 0) {    // boost
+                norm = 1 / (1 + sqrt(2) * K + K * K);
+                a0 = (1 + sqrt(2 * V) * K + V * K * K) * norm;
+                a1 = 2 * (V * K * K - 1) * norm;
+                a2 = (1 - sqrt(2 * V) * K + V * K * K) * norm;
+                b1 = 2 * (K * K - 1) * norm;
+                b2 = (1 - sqrt(2) * K + K * K) * norm;
+            } else {    // cut
+                norm = 1 / (1 + sqrt(2 * V) * K + V * K * K);
+                a0 = (1 + sqrt(2) * K + K * K) * norm;
+                a1 = 2 * (K * K - 1) * norm;
+                a2 = (1 - sqrt(2) * K + K * K) * norm;
+                b1 = 2 * (V * K * K - 1) * norm;
+                b2 = (1 - sqrt(2 * V) * K + V * K * K) * norm;
+            }
+            break;
+        case HIGHSHELF:
+            if (peakGain >= 0) {    // boost
+                norm = 1 / (1 + sqrt(2) * K + K * K);
+                a0 = (V + sqrt(2 * V) * K + K * K) * norm;
+                a1 = 2 * (K * K - V) * norm;
+                a2 = (V - sqrt(2 * V) * K + K * K) * norm;
+                b1 = 2 * (K * K - 1) * norm;
+                b2 = (1 - sqrt(2) * K + K * K) * norm;
+            } else {    // cut
+                norm = 1 / (V + sqrt(2 * V) * K + K * K);
+                a0 = (1 + sqrt(2) * K + K * K) * norm;
+                a1 = 2 * (K * K - 1) * norm;
+                a2 = (1 - sqrt(2) * K + K * K) * norm;
+                b1 = 2 * (K * K - V) * norm;
+                b2 = (V - sqrt(2 * V) * K + K * K) * norm;
+            }
+            break;
+    }
+}
---
+++ src/dsp/BiquadFilter.hpp
@@ -0,0 +1,74 @@
+/*                                                                     *\
+**       __   ___  ______                                              **
+**      / /  / _ \/_  __/                                              **
+**     / /__/ , _/ / /    Lindenberg                                   **
+**    /____/_/|_| /_/  Research Tec.                                   **
+**                                                                     **
+**                                                                     **
+**	  https://github.com/lindenbergresearch/LRTRack	                   **
+**    heapdump@icloud.com                                              **
+**		                                                               **
+**    Sound Modules for VCV Rack                                       **
+**    Copyright 2017/2018 by Patrick Lindenberg / LRT                  **
+**                                                                     **
+**    For Redistribution and use in source and binary forms,           **
+**    with or without modification please see LICENSE.                 **
+**                                                                     **
+\*                                                                     */
+
+#pragma once
+
+#include "DSPEffect.hpp"
+
+namespace dsp {
+
+enum BiquadType {
+    LOWPASS = 0,
+    HIGHPASS,
+    BANDPASS,
+    NOTCH,
+    PEAK,
+    LOWSHELF,
+    HIGHSHELF
+};
+
+
+/**
+ * @brief Common Biquad filters
+ * based on: https://www.earlevel.com/main/2012/11/26/biquad-c-source-code/
+ */
+struct Biquad : DSPEffect {
+public:
+    Biquad(float sr);
+    Biquad(BiquadType type, double Fc, double Q, double peakGainDB, float sr);
+    ~Biquad();
+    void setType(BiquadType type);
+    void setQ(double Q);
+    void setFc(double Fc);
+    void setPeakGain(double peakGainDB);
+
+
+    void setBiquad(BiquadType type, double Fc, double Q, double peakGain);
+    void process() override;
+    void invalidate() override;
+    void init() override;
+    float in, out;
+
+protected:
+
+
+    int type;
+    double a0, a1, a2, b1, b2;
+    double Fc, Q, peakGain;
+    double z1, z2;
+};
+
+
+inline void Biquad::process() {
+    out = in * a0 + z1;
+    z1 = in * a1 + z2 - b1 * out;
+    z2 = in * a2 - b2 * out;
+}
+
+
+}
