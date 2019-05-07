4b51aec737c7792ea09ce5617ce06d897300deae
patricklindenberg <patrick@heapdump.com>
2019-05-07 08:52:11 +0200

    initial commit

---
+++ src/dsp/BBDCore.hpp
@@ -0,0 +1,42 @@
+/*                                                                     *\
+**       __   ___  ______                                              **
+**      / /  / _ \/_  __/                                              **
+**     / /__/ , _/ / /    Lindenberg                                   **
+**    /____/_/|_| /_/  Research Tec.                                   **
+**                                                                     **
+**                                                                     **
+**	  https://github.com/lindenbergresearch/LRTRack	                   **
+**    heapdump@icloud.com                                              **
+**		                                                               **
+**    Sound Modules for VCV Rack                                       **
+**    Copyright 2017/2018 by Patrick Lindenberg / LRT                  **
+**                                                                     **
+**    For Redistribution and use in source and binary forms,           **
+**    with or without modification please see LICENSE.                 **
+**                                                                     **
+\*                                                                     */
+
+#pragma once
+
+namespace dsp {
+
+struct BBDBuffer {
+private:
+    float *data;
+    unsigned int index;
+
+public:
+
+    BBDBuffer(unsigned int size) {
+        data = new float[size];
+        index = 0;
+    }
+
+
+    virtual ~BBDBuffer() {
+        delete[] data;
+    }
+
+};
+
+}
