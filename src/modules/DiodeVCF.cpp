4128e1a323f990ecad7ca4eb7242a3dd059650f5
patricklindenberg <patrick@heapdump.com>
2019-05-23 10:38:31 +0200

    migrate to v1

--- src/modules/DiodeVCF.cpp
+++ src/modules/DiodeVCF.cpp
@@ -1,3 +1,4 @@
+#include <dsp/common.hpp>
 #include "../LindenbergResearch.hpp"
 #include "../dsp/DiodeLadder.hpp"
 #include "../dsp/Hardclip.hpp"
@@ -38,85 +39,56 @@ struct DiodeVCF : LRModule {
 
 
     DiodeVCF() : LRModule(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS) {
-        frqKnob = LRKnob::create<LRBigKnob>(Vec(32.5, 74.4), this, DiodeVCF::FREQUENCY_PARAM, 0.f, 1.f, 1.f);
-        resKnob = LRKnob::create<LRBigKnob>(Vec(151.5, 74.4), this, DiodeVCF::RES_PARAM, 0.0f, 1.0, 0.0f);
-        saturateKnob = LRKnob::create<LRMiddleKnob>(Vec(99.5, 164.4), this, DiodeVCF::SATURATE_PARAM, 0.f, 1.0, 0.0f);
+        configParam(FREQUENCY_PARAM, -1.f, 1.0f, 0.f);
+        configParam(RES_PARAM, 0.0f, 1.0, 0.0f);
+        configParam(SATURATE_PARAM, 0.f, 1.0, 0.0f);
+
+        configParam(FREQUENCY_CV_PARAM, -1.f, 1.0f, 0.f);
+        configParam(RESONANCE_CV_PARAM, -1.f, 1.0f, 0.f);
+        configParam(SATURATE_CV_PARAM, -1.f, 1.0f, 0.f);
     }
 
 
-    void onRandomize() override;
-    void updateComponents();
-
-    DiodeLadderFilter *lpf = new DiodeLadderFilter(engineGetSampleRate());
-
-    LRBigKnob *frqKnob = NULL;
-    LRBigKnob *resKnob = NULL;
-    LRMiddleKnob *saturateKnob = NULL;
-
+    DiodeLadderFilter *lpf = new DiodeLadderFilter(APP->engine->getSampleRate());
     LRPanel *panel;
 
     bool aged = false;
     bool hidef = false;
 
-
-    /* json_t *dataToJson() override {
-         json_t *rootJ = LRModule::dataToJson();
-
-         json_object_set_new(rootJ, "AGED", json_boolean(aged));
-         json_object_set_new(rootJ, "hidef", json_boolean(hidef));
-         return rootJ;
-     }
-
-
-     void dataFromJson(json_t *rootJ) override {
-         LRModule::dataFromJson(rootJ);
-
-         json_t *agedJ = json_object_get(rootJ, "AGED");
-         if (agedJ)
-             aged = json_boolean_value(agedJ);
-
-         json_t *hidefJ = json_object_get(rootJ, "hidef");
-         if (agedJ)
-             hidef = json_boolean_value(hidefJ);
-
-         updateComponents();
-     }*/
-
-
-    void step() override;
+    void process(const ProcessArgs &args) override;
     void onSampleRateChange() override;
 };
 
 
-void DiodeVCF::step() {
+void DiodeVCF::process(const ProcessArgs &args) {
     float freqcv = 0, rescv = 0, satcv = 0;
 
-    if (inputs[FREQUCENCY_CV_INPUT].active) {
-        freqcv = inputs[FREQUCENCY_CV_INPUT].value / 10 * quadraticBipolar(params[FREQUENCY_CV_PARAM].value);
+    if (inputs[FREQUCENCY_CV_INPUT].isConnected()) {
+        freqcv = inputs[FREQUCENCY_CV_INPUT].getVoltage() / 10 * dsp::quadraticBipolar(params[FREQUENCY_CV_PARAM].getValue());
     }
 
 
-    if (inputs[RESONANCE_CV_INPUT].active) {
-        rescv = inputs[RESONANCE_CV_INPUT].value / 10 * quadraticBipolar(params[RESONANCE_CV_PARAM].value);
+    if (inputs[RESONANCE_CV_INPUT].isConnected()) {
+        rescv = inputs[RESONANCE_CV_INPUT].getVoltage() / 10 * dsp::quadraticBipolar(params[RESONANCE_CV_PARAM].getValue());
     }
 
-    if (inputs[SATURATE_CV_INPUT].active) {
-        satcv = inputs[SATURATE_CV_INPUT].value / 10 * quadraticBipolar(params[SATURATE_CV_PARAM].value);
+    if (inputs[SATURATE_CV_INPUT].isConnected()) {
+        satcv = inputs[SATURATE_CV_INPUT].getVoltage() / 10 * dsp::quadraticBipolar(params[SATURATE_CV_PARAM].getValue());
     }
 
-    float frq = clamp(params[FREQUENCY_PARAM].value + freqcv, 0.f, 1.f);
-    float res = clamp((params[RES_PARAM].value + rescv) * DiodeLadderFilter::MAX_RESONANCE, 0.f, DiodeLadderFilter::MAX_RESONANCE);
-    float sat = clamp(quarticBipolar((params[SATURATE_PARAM].value) + satcv) * 14 + 1, 0.f, 15.f);
+    float frq = clamp(params[FREQUENCY_PARAM].getValue() + freqcv, 0.f, 1.f);
+    float res = clamp((params[RES_PARAM].getValue() + rescv) * DiodeLadderFilter::MAX_RESONANCE, 0.f, DiodeLadderFilter::MAX_RESONANCE);
+    float sat = clamp(dsp::quarticBipolar((params[SATURATE_PARAM].getValue()) + satcv) * 14 + 1, 0.f, 15.f);
 
-    if (frqKnob != nullptr && resKnob != nullptr && saturateKnob != nullptr) {
-        frqKnob->setIndicatorActive(inputs[FREQUCENCY_CV_INPUT].active);
-        resKnob->setIndicatorActive(inputs[RESONANCE_CV_INPUT].active);
-        saturateKnob->setIndicatorActive(inputs[SATURATE_CV_INPUT].active);
+    /*if (frqKnob != nullptr && resKnob != nullptr && saturateKnob != nullptr) {
+        frqKnob->setIndicatorActive(inputs[FREQUCENCY_CV_INPUT].isConnected());
+        resKnob->setIndicatorActive(inputs[RESONANCE_CV_INPUT].isConnected());
+        saturateKnob->setIndicatorActive(inputs[SATURATE_CV_INPUT].isConnected());
 
-        frqKnob->setIndicatorValue(params[FREQUENCY_PARAM].value + freqcv);
-        resKnob->setIndicatorValue(params[RES_PARAM].value + rescv);
-        saturateKnob->setIndicatorValue(params[SATURATE_PARAM].value + satcv);
-    }
+        frqKnob->setIndicatorValue(params[FREQUENCY_PARAM].getValue() + freqcv);
+        resKnob->setIndicatorValue(params[RES_PARAM].getValue() + rescv);
+        saturateKnob->setIndicatorValue(params[SATURATE_PARAM].getValue() + satcv);
+    }*/
 
     lpf->setFrequency(frq);
     lpf->setResonance(res);
@@ -124,34 +96,21 @@ void DiodeVCF::step() {
 
     lpf->low = !hidef;
 
-    lpf->setIn(inputs[FILTER_INPUT].value / 10.f);
+    lpf->setIn(inputs[FILTER_INPUT].getVoltage() / 10.f);
     lpf->invalidate();
     lpf->process();
 
     /* compensate gain drop on resonance inc.
-    float q = params[RES_PARAM].value * 1.8f + 1;*/
+    float q = params[RES_PARAM].getValue() * 1.8f + 1;*/
 
-    outputs[HP_OUTPUT].value = lpf->getOut2() * 6.5f;  // hipass
-    outputs[LP_OUTPUT].value = lpf->getOut() * 10.f;   // lowpass
-}
-
-
-void DiodeVCF::updateComponents() {
-
+    outputs[HP_OUTPUT].setVoltage(lpf->getOut2() * 6.5f);  // hipass
+    outputs[LP_OUTPUT].setVoltage(lpf->getOut() * 10.f);   // lowpass
 }
 
 
 void DiodeVCF::onSampleRateChange() {
     Module::onSampleRateChange();
-    lpf->setSamplerate(engineGetSampleRate());
-}
-
-
-void DiodeVCF::onRandomize() {
-    Module::randomize();
-    // patina->box.pos = Vec(-randomUniform() * 1000, -randomUniform() * 200);
-
-    updateComponents();
+    lpf->setSamplerate(APP->engine->getSampleRate());
 }
 
 
@@ -159,20 +118,22 @@ void DiodeVCF::onRandomize() {
  * @brief Blank Panel with Logo
  */
 struct DiodeVCFWidget : LRModuleWidget {
+    LRBigKnob *frqKnob, *resKnob;
+    LRMiddleKnob *saturateKnob;
+
     DiodeVCFWidget(DiodeVCF *module);
     void appendContextMenu(Menu *menu) override;
 };
 
 
 DiodeVCFWidget::DiodeVCFWidget(DiodeVCF *module) : LRModuleWidget(module) {
-    panel->addSVGVariant(LRGestalt::DARK, SVG::load(assetPlugin(pluginInstance, "res/panels/DiodeLadderVCFClassic.svg")));
-    panel->addSVGVariant(LRGestalt::LIGHT, SVG::load(assetPlugin(pluginInstance, "res/panels/DiodeLadderVCF.svg")));
-    panel->addSVGVariant(LRGestalt::AGED, SVG::load(assetPlugin(pluginInstance, "res/panels/DiodeLadderVCFAged.svg")));
+    panel->addSVGVariant(LRGestalt::DARK, APP->window->loadSvg(asset::plugin(pluginInstance, "res/panels/DiodeLadderVCFClassic.svg")));
+    panel->addSVGVariant(LRGestalt::LIGHT, APP->window->loadSvg(asset::plugin(pluginInstance, "res/panels/DiodeLadderVCF.svg")));
+    panel->addSVGVariant(LRGestalt::AGED, APP->window->loadSvg(asset::plugin(pluginInstance, "res/panels/DiodeLadderVCFAged.svg")));
 
     panel->init();
     addChild(panel);
 
-    module->panel = panel;
     box.size = panel->box.size;
 
     // ***** SCREWS **********
@@ -183,91 +144,40 @@ DiodeVCFWidget::DiodeVCFWidget(DiodeVCF *module) : LRModuleWidget(module) {
     // ***** SCREWS **********
 
     // ***** MAIN KNOBS ******
-    module->frqKnob->setIndicatorColors(nvgRGBAf(0.9f, 0.9f, 0.9f, 1.0f));
-    module->resKnob->setIndicatorColors(nvgRGBAf(0.9f, 0.9f, 0.9f, 1.0f));
-    module->saturateKnob->setIndicatorColors(nvgRGBAf(0.9f, 0.9f, 0.9f, 1.0f));
+    frqKnob = createParam<LRBigKnob>(Vec(32.5, 74.4), module, DiodeVCF::FREQUENCY_PARAM);
+    resKnob = createParam<LRBigKnob>(Vec(151.5, 74.4), module, DiodeVCF::RES_PARAM);
+    saturateKnob = createParam<LRMiddleKnob>(Vec(99.5, 164.4), module, DiodeVCF::SATURATE_PARAM);
 
-    addParam(module->frqKnob);
-    addParam(module->resKnob);
-    addParam(module->saturateKnob);
+    frqKnob->setIndicatorColors(nvgRGBAf(0.9f, 0.9f, 0.9f, 1.0f));
+    resKnob->setIndicatorColors(nvgRGBAf(0.9f, 0.9f, 0.9f, 1.0f));
+    saturateKnob->setIndicatorColors(nvgRGBAf(0.9f, 0.9f, 0.9f, 1.0f));
 
-    addParam(ParamcreateWidget<LRSmallKnob>(Vec(39.9, 251.4), module, DiodeVCF::FREQUENCY_CV_PARAM, -1.f, 1.0f, 0.f));
-    addParam(ParamcreateWidget<LRSmallKnob>(Vec(177, 251.4), module, DiodeVCF::RESONANCE_CV_PARAM, -1.f, 1.0f, 0.f));
-    addParam(ParamcreateWidget<LRSmallKnob>(Vec(108.5, 251.4), module, DiodeVCF::SATURATE_CV_PARAM, -1.f, 1.0f, 0.f));
+    addParam(frqKnob);
+    addParam(resKnob);
+    addParam(saturateKnob);
+
+    addParam(createParam<LRSmallKnob>(Vec(39.9, 251.4), module, DiodeVCF::FREQUENCY_CV_PARAM));
+    addParam(createParam<LRSmallKnob>(Vec(177, 251.4), module, DiodeVCF::RESONANCE_CV_PARAM));
+    addParam(createParam<LRSmallKnob>(Vec(108.5, 251.4), module, DiodeVCF::SATURATE_CV_PARAM));
     // ***** MAIN KNOBS ******
 
     // ***** CV INPUTS *******
-    addInput(createPort<LRIOPortCV>(Vec(37.4, 284.4), PortWidget::INPUT, module, DiodeVCF::FREQUCENCY_CV_INPUT));
-    addInput(createPort<LRIOPortCV>(Vec(175.3, 284.4), PortWidget::INPUT, module, DiodeVCF::RESONANCE_CV_INPUT));
-    addInput(createPort<LRIOPortCV>(Vec(106.4, 284.4), PortWidget::INPUT, module, DiodeVCF::SATURATE_CV_INPUT));
+    addInput(createInput<LRIOPortCV>(Vec(37.4, 284.4), module, DiodeVCF::FREQUCENCY_CV_INPUT));
+    addInput(createInput<LRIOPortCV>(Vec(175.3, 284.4), module, DiodeVCF::RESONANCE_CV_INPUT));
+    addInput(createInput<LRIOPortCV>(Vec(106.4, 284.4), module, DiodeVCF::SATURATE_CV_INPUT));
     // ***** CV INPUTS *******
 
 
     // ***** INPUTS **********
-    addInput(createPort<LRIOPortAudio>(Vec(37.4, 318.5), PortWidget::INPUT, module, DiodeVCF::FILTER_INPUT));
+    addInput(createInput<LRIOPortAudio>(Vec(37.4, 318.5), module, DiodeVCF::FILTER_INPUT));
     // ***** INPUTS **********
 
     // ***** OUTPUTS *********
-    addOutput(createPort<LRIOPortAudio>(Vec(175.3, 318.5), PortWidget::OUTPUT, module, DiodeVCF::LP_OUTPUT));
-    addOutput(createPort<LRIOPortAudio>(Vec(106.4, 318.5), PortWidget::OUTPUT, module, DiodeVCF::HP_OUTPUT));
+    addOutput(createOutput<LRIOPortAudio>(Vec(175.3, 318.5), module, DiodeVCF::LP_OUTPUT));
+    addOutput(createOutput<LRIOPortAudio>(Vec(106.4, 318.5), module, DiodeVCF::HP_OUTPUT));
     // ***** OUTPUTS *********
 }
 
+//TODO: [2019-05-23 10:32] => recover oversampling menu
 
-/*
-struct DiodeVCFAged : MenuItem {
-    DiodeVCF *diodeVCF;
-
-
-    void onAction(EventAction &e) override {
-        if (diodeVCF->aged) {
-            diodeVCF->aged = false;
-        } else {
-            diodeVCF->aged = true;
-        }
-
-        diodeVCF->updateComponents();
-    }
-
-
-    void step() override {
-        rightText = CHECKMARK(diodeVCF->aged);
-    }
-};
-*/
-
-struct DiodeVCFHiDef : MenuItem {
-    DiodeVCF *diodeVCF;
-
-
-    void onAction(EventAction &e) override {
-        if (diodeVCF->hidef) {
-            diodeVCF->hidef = false;
-        } else {
-            diodeVCF->hidef = true;
-        }
-
-        diodeVCF->updateComponents();
-    }
-
-
-    void step() override {
-        rightText = CHECKMARK(diodeVCF->hidef);
-    }
-};
-
-
-void DiodeVCFWidget::appendContextMenu(Menu *menu) {
-    menu->addChild(MenuEntry::create());
-
-    DiodeVCF *diodeVCF = dynamic_cast<DiodeVCF *>(module);
-    assert(diodeVCF);
-
-
-    DiodeVCFHiDef *mergeItemHiDef = createMenuItem<DiodeVCFHiDef>("Use 4x oversampling");
-    mergeItemHiDef->diodeVCF = diodeVCF;
-    menu->addChild(mergeItemHiDef);
-}
-
-
-Model *modelDiodeVCF = createModel<DiodeVCF, DiodeVCFWidget>("Lindenberg Research", "DIODE VCF", "Laika Diode-Ladder Filter", FILTER_TAG);
+Model *modelDiodeVCF = createModel<DiodeVCF, DiodeVCFWidget>("DIODE VCF");
