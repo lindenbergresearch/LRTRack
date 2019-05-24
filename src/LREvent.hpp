0f8552f2035ad95f98860b711abe537739ffc1c2
patricklindenberg <patrick@heapdump.com>
2019-05-24 17:26:38 +0200

    initial commit

---
+++ src/LREvent.hpp
@@ -0,0 +1,53 @@
+/*                                                                     *\
+**       __   ___  ______                                              **
+**      / /  / _ \/_  __/                                              **
+**     / /__/ , _/ / /    Lindenberg                                   **
+**    /____/_/|_| /_/  Research Tec.                                   **
+**                                                                     **
+**                                                                     **
+**	  https://github.com/lindenbergresearch/LRTRack	                   **
+**    heapdump@icloud.com                                              **
+**		                                                               **
+**    Sound Modules for VCV Rack                                       **
+**    Copyright 2017-2019 by Patrick Lindenberg / LRT                  **
+**                                                                     **
+**    For Redistribution and use in source and binary forms,           **
+**    with or without modification please see LICENSE.                 **
+**                                                                     **
+\*                                                                     */
+#pragma once
+
+#include "LRGestalt.hpp"
+
+namespace lrt {
+
+/**
+ * @brief Virtual Event baseclass
+ */
+struct LREvent {
+    /** marker for consumption */
+    bool consumed = false;
+
+
+    /**
+     * @brief
+     */
+    virtual void consume() { consumed = true; }
+};
+
+
+/**
+ * @brief Virtual Action baseclass
+ */
+struct LREventAction {
+
+    /**
+     * @brief To be overridden event catcher
+     * @param e
+     */
+    virtual void onEventAction(LREvent *e);
+};
+
+
+}
+
