f2f7636b2827f27d295e84498595aff8e95c08e7
patricklindenberg <patrick@heapdump.com>
2019-05-29 16:08:13 +0200

    add header

--- src/dsp/MS20zdf.hpp
+++ src/dsp/MS20zdf.hpp
@@ -1,3 +1,20 @@
+/*                                                                     *\
+**       __   ___  ______                                              **
+**      / /  / _ \/_  __/                                              **
+**     / /__/ , _/ / /    Lindenberg                                   **
+**    /____/_/|_| /_/  Research Tec.                                   **
+**                                                                     **
+**                                                                     **
+**	  https://github.com/lindenbergresearch/LRTRack	                   **
+**    heapdump@icloud.com                                              **
+**		                                                               **
+**    Sound Modules for VCV Rack                                       **
+**    Copyright 2017-2019 by Patrick Lindenberg / LRT                  **
+**                                                                     **
+**    For Redistribution and use in source and binary forms,           **
+**    with or without modification please see LICENSE.                 **
+**                                                                     **
+\*                                                                     */
 #pragma once
 
 #include "DSPSystem.hpp"
