4109bab0fbf951403a6eb2908e3f07ff3e2249a8
patricklindenberg <patrick@heapdump.com>
2019-06-03 17:00:25 +0200

    add debug mode hint in log

--- src/LindenbergResearch.cpp
+++ src/LindenbergResearch.cpp
@@ -6,7 +6,6 @@ Plugin *pluginInstance;
 void init(Plugin *p) {
     pluginInstance = p;
 
-
     p->addModel(modelType35);
     p->addModel(modelDiodeVCF);
     p->addModel(modelBlankPanel);
@@ -19,6 +18,11 @@ void init(Plugin *p) {
     p->addModel(modelAlmaFilter);
     p->addModel(modelMS20Filter);
     p->addModel(modelWestcoast);
+
+    #ifdef LRT_DEBUG
+    DEBUG("Operating in DEBUG mode...");
+    #endif
+
 
 /*
 
